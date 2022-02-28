639a589c9f6d7bddb09c6a46215fb21152d942e5
Oliver <kollo.oliver@gmail.com>
2022-02-28 14:47:17 +0000

    condensed the script into 3 methods to get data

--- demo2.py
+++ demo2.py
@@ -4,3 +4,4 @@ bot = scraper2.Scraper('https://ftx.com/markets')
 
 bot.find_all_links()
 bot.valid_links()
+bot.get_data()
--- scraper2.py
+++ scraper2.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 import time, uuid, urllib, os, requests
 
 class Scraper:
@@ -18,25 +19,50 @@ class Scraper:
     
     def find_all_links(self):
         '''Finds elements on website with //a[@href] and compiles a list called (all_url)'''
-        all_url = []
+        self.all_url = []
         try:
             WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//a[@href]")))
             loop = self.driver.find_elements_by_xpath("//a[@href]")
             for links in loop:
-                all_url.append(links.get_attribute("href"))
+                self.all_url.append(links.get_attribute("href"))
         except:
             print("No links found. Website might not have loaded correctly. Try again.")
-        return all_url
+        return self.all_url
 
-    def valid_links(self, all_url):
-        '''Aims to clean the list (all_url) and get the relevant links (valid_url)'''
-        valid_url = []
-        for i in all_url:
+    def valid_links(self):
+        '''Aims to clean the list (all_url) and get the relevant links (valid_url).'''
+        self.valid_url = []
+        for i in self.all_url:
             if "trade" and "-" in i:
-                valid_url.append(i)
-        del valid_url[-5:]
-        del valid_url[:2]
-        return valid_url
+                self.valid_url.append(i)
+        del self.valid_url[-5:]
+        del self.valid_url[:2]
+        return self.valid_url
 
     def get_data(self):
-        
+        '''This method will get data such as price,name,link,uuid and create a dictionary.'''
+        dictionary = {
+                    'UUID':[],
+                    'Link':[],
+                    'Name':[],
+                    'Price':[]
+                    }
+        for links in self.valid_url:
+            self.driver.get(links)
+            time.sleep(2)
+            try:
+                value = self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div/main/div[3]/div[3]/div/div/div/span[1]/p[2]").text
+                dictionary['Price'].append(value)
+            except NoSuchElementException:
+                dictionary['Price'].append('N/A')
+            try:
+                dictionary['Link'].append(links)
+            except NoSuchElementException:
+                dictionary['Link'].append('N/A')
+            try:
+                splitted = links.split("/")[-1]
+                dictionary['Name'].append(splitted)
+            except NoSuchElementException:
+                dictionary['Name'].append('N/A')
+            links = str(uuid.uuid4())
+            dictionary['UUID'].append(links)
